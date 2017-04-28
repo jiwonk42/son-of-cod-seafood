8061e49b646bf8ab449e4a7e7b5cb8b348e59bec
Jiwon Kang <jiwonk42@gmail.com>
2017-04-28 11:40:02 -0700

    add Registration

--- src/SonOfCodSeafood/Controllers/AccountController.cs
+++ src/SonOfCodSeafood/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using SonOfCodSeafood.Models;
+using SonOfCodSeafood.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -26,6 +27,26 @@ namespace SonOfCodSeafood.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var user = new ApplicationUser { UserName = model.Email };
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
