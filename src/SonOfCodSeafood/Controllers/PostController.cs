e5457f1c2bc6d7a5dc8df2028ae0d50ce670ef01
Jiwon Kang <jiwonk42@gmail.com>
2017-04-28 12:33:51 -0700

    add Post Controller that manages posts

---
+++ src/SonOfCodSeafood/Controllers/PostController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using SonOfCodSeafood.Models;
+using Microsoft.AspNetCore.Identity;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SonOfCodSeafood.Controllers
+{
+    [Authorize]
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public PostController(UserManager<ApplicationUser> userManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
