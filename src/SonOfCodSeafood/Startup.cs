6ae28a92942264dff19606094e48788e6c8cdedd
Jiwon Kang <jiwonk42@gmail.com>
2017-04-28 15:20:14 -0700

    add a Nav bar

--- src/SonOfCodSeafood/Startup.cs
+++ src/SonOfCodSeafood/Startup.cs
@@ -45,6 +45,7 @@ namespace SonOfCodSeafood
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseStaticFiles();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
