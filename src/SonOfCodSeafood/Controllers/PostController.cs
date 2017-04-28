1b256e1a7a0fe932b4d204080d86c2726d03fcd7
Jiwon Kang <jiwonk42@gmail.com>
2017-04-28 13:15:07 -0700

    add Edit functionality for Posts

--- src/SonOfCodSeafood/Controllers/PostController.cs
+++ src/SonOfCodSeafood/Controllers/PostController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using SonOfCodSeafood.Models;
 using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -42,6 +43,20 @@ namespace SonOfCodSeafood.Controllers
             var currentUser = await _userManager.FindByIdAsync(userId);
             post.User = currentUser;
             _db.Posts.Add(post);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisItem = _db.Posts.FirstOrDefault(posts => posts.Id == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            _db.Entry(post).State = EntityState.Modified;
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
